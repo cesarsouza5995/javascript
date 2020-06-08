0c0b7cb37ccaca7f8297bb9492bb9607b8129d05
Cesar Souza <cesarsouza5995@gmail.com>
2020-06-08 11:03:20 -0300

    Create aula11ex08.js
    condição simples

---
+++ euFaço/aula11ex08.js
@@ -0,0 +1,6 @@
+var vel = 20.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+    if (vel > 60) { //condição simples, só tem um if, não tem o else
+console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+    }
+console.log(`Dirija sempre usando cinto de segurança`)
